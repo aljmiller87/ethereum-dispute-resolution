a238038c16778ddaabbb09472ebd8e9e96574c1d
Alex Miller <aljmiller87@gmail.com>
2020-06-06 09:05:03 -0400

    Removed legacy contractReducer and related actions file.
    [account] page ui is not updating by moving contracts to correct list after successful event listen and contract state detail update. This is because:
     *[account] page is only ever passing the currentView.contracts array to ContractGrid and this array is not changing
    
    One possible solution is to have the ContractGrid or ContractList useSelector the Contract details so they re-render when the contractDetails slice of the redux store updates

--- redux/reducers/rootReducer.js
+++ redux/reducers/rootReducer.js
@@ -1,7 +1,6 @@
 import { combineReducers } from "redux";
 import networkReducer from "./networkReducer";
 import accountReducer from "./accountReducer";
-import contractReducer from "./contractReducer";
 import blockchainCallsReducer from "./blockchainStatusReducer";
 import dashboardReducer from "./dashboardReducer";
 import contractDetails from "./contractDetails";
@@ -11,7 +10,6 @@ import contractSubscribe from "./contractSubscribe";
 const rootReducer = combineReducers({
   accountReducer,
   networkReducer,
-  contractReducer,
   blockchainCallsReducer,
   dashboardReducer,
   contractDetails,
